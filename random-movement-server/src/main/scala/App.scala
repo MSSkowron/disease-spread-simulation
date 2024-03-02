5031be5ee874ecec79205ab3e44460cda9116cd2
MSSkowron <mateusz.skowron@poczta.onet.pl>
2024-03-02 23:10:40 +0100

    Add CORS

--- random-movement-server/src/main/scala/App.scala
+++ random-movement-server/src/main/scala/App.scala
@@ -2,6 +2,8 @@ import cats.effect.{ExitCode, IO, IOApp}
 import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.server.middleware.{CORS, Logger}
+
 import scala.concurrent.ExecutionContext.global
 
 object App extends IOApp{
@@ -12,7 +14,7 @@ object App extends IOApp{
 
     BlazeServerBuilder[IO](global)
       .bindHttp(8080, "localhost")
-      .withHttpApp(apis)
+      .withHttpApp(Logger.httpApp(logHeaders = true, logBody = true)(CORS(apis)))
       .resource
       .use(_ => IO.never)
       .as(ExitCode.Success)
